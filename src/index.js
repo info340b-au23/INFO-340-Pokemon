2acd82107b80e61d900335acc2ca23ffbab83622
ShiYiShe <yshi6@uw.edu>
2023-11-22 18:24:00 -0800

    import form

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './search-components/search-App';
 // import Collection from './collection-components/collection-App';
 // import Cooking from './cooking-components/Cooking-App';
+// import Form from './form-components/Form';
 import data from './data/pokemon.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,4 +12,5 @@ root.render(
     <App pokemon={data}/>
     // <Collection pokemon={data}/>
     // <Cooking />
+    // <Form />
 );
