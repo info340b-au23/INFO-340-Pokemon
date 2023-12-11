4e29f6d5698b5831150f8cec109bb36553d9ff37
ShiYiShe <yshi6@uw.edu>
2023-12-11 15:56:50 -0800

    cooking debug

--- src/cooking-components/Cooking-App.js
+++ src/cooking-components/Cooking-App.js
@@ -35,14 +35,14 @@ function App() {
   const determineDish = (ingredients) => {
     const { fancyApple, fancyEgg, honey, moomooMilk, snoozyTomato, soothingCacao, warmingGinger } = ingredients;
 
-    if (moomooMilk === 7) return 'Warm Moomoo Milk';
     if (honey === 9 && soothingCacao === 8 && moomooMilk === 7) return 'Sweet Scent Chocolate Cake';
     if (fancyApple === 11 && moomooMilk === 9 && honey === 7 && soothingCacao === 8) return 'Lovely Kiss Smoothie';
     if (honey === 20 && fancyEgg === 15 && moomooMilk === 10 && fancyApple === 10) return 'Jigglypuff\'s Fruity Flan';
-    if (honey === 9) return 'Craft Soda Pop';
     if (honey === 14 && warmingGinger === 12 && soothingCacao === 5 && fancyEgg === 4) return 'Steadfast Ginger Cookies';
     if (snoozyTomato === 9 && fancyApple === 7) return 'Stalwart Vegetable Juice';
     if (warmingGinger === 9 && fancyApple === 7) return 'Ember Ginger Tea';
+    if (moomooMilk === 7) return 'Warm Moomoo Milk';
+    if (honey === 9) return 'Craft Soda Pop';
 
     return null;
   };
